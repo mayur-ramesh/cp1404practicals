3c7c7cddd159195dffa8c2bb310b068762f53c64
mayur-ramesh <mayuramesh1512@gmail.com>
2025-02-08 20:50:54 +0800

    refactored to functions added doc strings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,17 +1,20 @@
 PASSWORD_LENGTH =10
 
 def main():
+    """main function"""
     password = get_password()
 
     get_asterisk(password)
 
 
 def get_asterisk(password):
+    """get number of asterisks"""
     length = len(password)
     print(f"{length * "*"}")
 
 
 def get_password():
+    """gets valid password"""
     password = input("Enter password: ")
     while len(password) <= PASSWORD_LENGTH:
         print("Invalid Password")
